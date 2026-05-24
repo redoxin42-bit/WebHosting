02f48ff01017b6e4597bf4de530077e368557276
redoxin42-bit <redoxin42@gmail.com>
2026-05-24 14:23:04 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        liquidBlue: '#00d4ff', // Базовый синий для LiquidGlass
+      },
+      backdropBlur: {
+        xs: '10px',
+      }
+    }
+  }
+}
